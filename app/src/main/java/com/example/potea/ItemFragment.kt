6214d6d852a469bbe8f6ad4bf2ab4707f98e1c3d
PC681 <shoxruxsharipov1105@gmail.com>
2023-04-15 14:03:50 +0500

    4 commit

--- app/src/main/java/com/example/potea/ItemFragment.kt
+++ app/src/main/java/com/example/potea/ItemFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.potea.databinding.FragmentItemBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -26,7 +27,9 @@ class ItemFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_item, container, false)
+        val binding = FragmentItemBinding.inflate(inflater,container,false)
+
+        return binding.root
     }
 
     companion object {
